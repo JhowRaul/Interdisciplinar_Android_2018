0f953bed11dfa33e26e29de474f914265a10276b
opet <opet@Academico.edu>
2018-04-25 11:01:16 -0300

    Base

---
+++ app/src/main/java/com/example/opet/interdisciplinar_android_2018/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.opet.interdisciplinar_android_2018;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
